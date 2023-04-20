5a83536bcf6ff1bcc90ac8c386460972d96798df
Fausto Carvalho <faustocarva@gmail.com>
2023-04-20 15:42:27 -0300

    place some main usage, give the graph lib the pub modifier and others

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,12 @@
+use graphlib::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut g = Graph::new();
+    g.add_vertex(1);
+    g.add_vertex(2);
+    g.add_vertex(3);
+    g.add_edge(1, 2);
+    g.add_edge(2, 3);
+    g.add_edge(3, 1);
+    println!("{:?}", g);
 }
--- graphlib/src/lib.rs
+++ graphlib/src/lib.rs
@@ -1,26 +1,2 @@
-use std::collections::HashMap;
-
-#[derive(Debug)]
-struct Graph {
-    adj_list: HashMap<u32, Vec<u32>>,
-}
-
-impl Graph {
-    fn new() -> Self {
-        Graph {
-            adj_list: HashMap::new(),
-        }
-    }
-
-    fn add_vertex(&mut self, v: u32) {
-        self.adj_list.entry(v).or_insert(Vec::new());
-    }
-
-    fn add_edge(&mut self, u: u32, v: u32) {
-        self.adj_list.entry(u).or_insert(Vec::new()).push(v);
-    }
-
-    fn get_adjacent_vertices(&self, v: u32) -> Option<&Vec<u32>> {
-        self.adj_list.get(&v)
-    }
-}
+pub use graph::*;
+mod graph;

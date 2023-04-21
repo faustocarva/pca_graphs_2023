02c629c45e563723ff1595d4794382fb52997399
Fausto Carvalho <faustocarva@gmail.com>
2023-04-21 12:35:14 -0300

    some unit tests

--- graphlib/src/graph.rs
+++ graphlib/src/graph.rs
@@ -24,3 +24,29 @@ impl Graph {
         self.adj_list.get(&v)
     }
 }
+
+
+#[cfg(test)]
+mod test_graph {
+    use super::Graph;
+    #[test]
+    fn test_add_vertex() {
+        let mut g = Graph::new();
+        g.add_vertex(1);        
+    }
+
+    #[test]
+    fn test_add_edges() {
+        let mut g = Graph::new();
+        g.add_vertex(1);
+        g.add_vertex(2);
+        g.add_vertex(3);        
+        g.add_edge(1, 2);
+        g.add_edge(2, 3);
+        println!("{:?}", g);
+        assert_eq!(
+            g.get_adjacent_vertices(2).unwrap(),
+            &vec![3]
+        );        
+    }
+}

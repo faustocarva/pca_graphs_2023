465e564487814d25ad38a02372cff2c1d434d368
Fausto Carvalho <faustocarva@gmail.com>
2023-04-23 18:32:12 -0300

    tests and first try dfs

--- graphlib/src/search.rs
+++ graphlib/src/search.rs
@@ -27,22 +27,20 @@ pub fn breadth_first_search(graph: &Graph, start: u32, target: u32) -> Option<Ve
 
 pub fn depth_first_search(graph: &Graph, start: u32, target: u32) -> Option<Vec<u32>> {
     let mut visited = HashSet::new();
-    let mut queue = Vec::new();
+    let mut queue = VecDeque::new();
     let mut result = Vec::new();
-    queue.push(start);
-    while let Some(node) = queue.pop() {
-        if visited.contains(&node) {
-            continue;
-        }        
-        visited.insert(node);
+    queue.push_back(start);
+    while let Some(node) = queue.pop_front() {
         result.push(node);
 
         if node == target {
             return Some(result);
         }        
         if let Some(neighbors) = graph.adj_list().get(&node) {
-            for neighbor in neighbors {
-                queue.push(*neighbor);
+            for neighbor in neighbors.into_iter().rev() {            
+                if visited.insert(*neighbor) {
+                    queue.push_front(*neighbor);
+                }
             }
         }
     }
@@ -51,8 +49,8 @@ pub fn depth_first_search(graph: &Graph, start: u32, target: u32) -> Option<Vec<
 
 
 #[cfg(test)]
-mod test_bfs {
-    
+mod test_search {
+
     #[test]
     fn test_bfs_find() {
         {
@@ -113,7 +111,7 @@ mod test_bfs {
     }
 
     #[test]
-    fn test_dfs_find_none() {        
+    fn test_dfs_find_none() {
         let mut graph = super::Graph::new();
         graph.add_vertex(1);
         graph.add_vertex(2);
@@ -124,6 +122,46 @@ mod test_bfs {
             super::depth_first_search(&graph, 1, 4).is_none(),
             true
         );
+
+    }
+
+    #[test]        
+    fn test_dfs_find() {
+        {
+            let mut graph = super::Graph::new();
+            graph.add_vertex(1);
+            graph.add_vertex(2);
+            graph.add_vertex(3);
+            graph.add_edge(1, 2);
+            graph.add_edge(2, 3);
+            assert_eq!(
+                super::depth_first_search(&graph, 1, 4).is_none(),
+                true
+            );    
+        }    
+        {
+            let mut graph = super::Graph::new();
+            graph.add_vertex(1);
+            graph.add_vertex(2);
+            graph.add_vertex(3);
+            graph.add_vertex(4);
+            graph.add_vertex(5);
+            graph.add_vertex(6);
+            graph.add_vertex(7);
+            graph.add_edge(1, 2);
+            graph.add_edge(1, 3);
+            graph.add_edge(2, 4);
+            graph.add_edge(2, 5);
+            graph.add_edge(3, 6);
+            graph.add_edge(3, 7);
+
+            println!("{:?}", graph);                    
+            
+            let result = super::depth_first_search(&graph, 1, 7);
+            let expected_path = vec![1, 2, 4, 5, 3, 6, 7];
+            assert_eq!(result, Some(expected_path));
+        }
+
 
     }
 
